c5238c9f837afc4f9868e5bf451016111e7bdabb
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2014-10-08 10:20:55 +0200

    Added pulsating isoid

---
+++ Assets/Pulse.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+	public float speed = 0.001f;
+	protected float multiplier = 0.5f;
+	public float maxSize = 1.0f;
+	public float minSize = 0.5f;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//transform.localScale.Scale(new Vector3 (speed, speed, speed));
+		//if (Time.realtimeSinceStartup % 3 == 0) {
+		multiplier += Time.deltaTime * speed;
+		transform.localScale = Vector3.one * multiplier;
+		if (multiplier > maxSize) {
+			multiplier = minSize;
+		}
+
+
+	}
+}
