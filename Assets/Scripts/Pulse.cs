35e5d52d76f676d2936483830219ee235faf1aee
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2014-10-09 08:57:55 +0200

    Polished the prototype

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -4,9 +4,6 @@ using System.Collections;
 public class Pulse : MonoBehaviour {
 
 	public float speed = 1f;
-	//protected float multiplier = 0.5f;
-	//public float maxSize = 1.0f;
-	//public float minSize = 0.5f;
 
     protected float counter = 0;
     protected float multiplier;
@@ -15,17 +12,6 @@ public class Pulse : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		//transform.localScale.Scale(new Vector3 (speed, speed, speed));
-		//if (Time.realtimeSinceStartup % 3 == 0) {
-
-        /*
-		multiplier += Time.deltaTime * speed;
-		transform.localScale = Vector3.one * multiplier;
-		if (multiplier > maxSize) {
-			multiplier = minSize;
-		}
-        */
-
         counter += Time.deltaTime * speed;
         multiplier = (Mathf.Sin(counter) * pulseSize) + averageSize;
         transform.localScale = Vector3.one * multiplier;
