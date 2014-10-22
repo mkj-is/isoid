3937d75672307218f282dc7552b9c0b62713774b
Karel Lockenbauer <karel.l@seznam.cz>
2014-10-22 22:07:55 +0200

    Dodatek k předchozímu.

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -8,7 +8,7 @@ public class Shooting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("left ctrl")) {
+		if (Input.GetKeyDown ("s")) {
 			Fire ();
 		}
 	}
