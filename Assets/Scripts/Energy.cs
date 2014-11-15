8ea3eff65a0ca395a6f2428152fc79427e9263c0
vinklerradomir <vinkler.radomir@gmail.com>
2014-11-15 13:55:59 +0100

    Ubyvani energie
    Isoid - energie ubyva porad a pomalu (jeste se nezmensuje velikost
    isoidu, na to jsem zapomnel)
    Player - energie ubyva pouze kdyz drzi sipku dopredu/dozadu (ma zapnute
    hlavni motory)

--- Assets/Scripts/Energy.cs
+++ Assets/Scripts/Energy.cs
@@ -6,6 +6,8 @@ public class Energy : MonoBehaviour {
 	public float StartingEnergy=100;
 	public float CurrentEnergy;
 
+    public float energyDecreaseRate = 1f;
+
 	// Use this for initialization
 	void Start () {
 		CurrentEnergy = StartingEnergy;
@@ -13,12 +15,25 @@ public class Energy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		//Destrukce při nulové energii
 		if (GetComponent<Energy> ().CurrentEnergy <= 0) {
 			Destroy ();
 		}
 	}
-	
+
+    void FixedUpdate()
+    {
+        if (this.gameObject.tag == "Player")
+        {
+            CurrentEnergy -= energyDecreaseRate * Time.deltaTime * Mathf.Abs(Input.GetAxisRaw("Vertical"));
+        }
+        else
+        {
+            CurrentEnergy -= energyDecreaseRate * Time.deltaTime;
+        }
+    }
+
 	void Destroy(){
 		Destroy (gameObject);
 	}
