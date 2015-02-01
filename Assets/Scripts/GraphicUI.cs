e2441b5e48fd99eed90c4fd1ed0ad6d1aa326875
Matěj Kašpar Jirásek <matej.jirasek@gmail.com>
2015-02-01 23:04:39 +0100

    Added simple energy bar

--- Assets/Scripts/GraphicUI.cs
+++ Assets/Scripts/GraphicUI.cs
@@ -15,8 +15,8 @@ public class GraphicUI : MonoBehaviour {
 	void OnGUI()
 	{
 		if (!gameOver) {
-			GUI.Label (new Rect (25, 25, 100, 30), Player.GetComponent<Energy> ().CurrentEnergy.ToString ("F2"));
-			GUI.Label (new Rect (25, 55, 100, 30), Time.realtimeSinceStartup.ToString ("F1"));
+			//GUI.Label (new Rect (25, 25, 100, 30), Player.GetComponent<Energy> ().CurrentEnergy.ToString ("F2"));
+			//GUI.Label (new Rect (25, 55, 100, 30), Time.realtimeSinceStartup.ToString ("F1"));
 		} else {
 			guiLay.fontSize=35;
 			GUI.Label (new Rect ((Screen.width/2)-100,(Screen.height/2)-100,200,80),"GAME OVER", guiLay);
